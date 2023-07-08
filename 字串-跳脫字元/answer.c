06b930a2396d11d54350b6ca0efea2b2cb7f981d
HuangJyunYu <yillkid@gmail.com>
2023-07-08 10:19:46 +0800

    Update answer.c

--- 字串-跳脫字元/answer.c
+++ 字串-跳脫字元/answer.c
@@ -6,7 +6,13 @@ int main() {
     while (1) {
         printf("請輸入一個字串（輸入 \"exit\" 結束）：");
         scanf("%s", str);
-        printf("%s\n", str);
+
+        int i = 0;
+        while (str[i] != '\0') {
+            printf("%c\n", str[i]);
+            i++;
+        }
+        printf("\n");
 
         if (str[0] == 'e' && str[1] == 'x' && str[2] == 'i' && str[3] == 't' && str[4] == '\0') {
             printf("程式結束。\n");
