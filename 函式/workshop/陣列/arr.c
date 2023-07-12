9388993bb8f61c634a4a0711c5f2af1a1d2df1ed
HuangJyunYu <yillkid@gmail.com>
2023-07-12 23:43:52 +0800

    Update arr.c

--- 函式/workshop/陣列/arr.c
+++ 函式/workshop/陣列/arr.c
@@ -8,7 +8,7 @@ void set_array_zero(int arr[], int size) {
 
 int main() {
     int numbers[] = {1, 2, 3, 4, 5};
-    int size = sizeof(numbers) / sizeof(numbers[0]);
+    int size = 5;//sizeof(numbers) / sizeof(numbers[0]);
 
     printf("原始陣列：\n");
     for (int i = 0; i < size; i++) {
